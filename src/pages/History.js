06bc87aa80a6bba181ad3eb0e5c0dcb7bab51f58
JinjuNa <48050357+JinjuNa@users.noreply.github.com>
2020-05-05 15:02:34 +0900

    school, history timeline 반응형 작업

--- src/pages/History.js
+++ src/pages/History.js
@@ -10,62 +10,75 @@ function History() {
         <SubTitle title={title} />
         <div className="history">
         <div className="contents">
-            <div className="historyImg">
-            <img src="../assets/history.png" alt="history" />
+            <div className="timeline">
+            <div className="start">OUR START</div>
+            <div className="timeline-container right">
+                <div className="timeline-content">
+                <h2>2012</h2>
+                <ul>
+                    <li>MENTORING-LAB설립</li>
+                </ul>
+                </div>
             </div>
-            <div className="gray">OUR START</div>``
-            <div className="yearBox right">
-            <h2>2012</h2>
-            <ul>
-                <li>MENTORING-LAB설립</li>
-            </ul>
+            <div className="timeline-container left">
+                <div className="timeline-content">
+                <h2>2013</h2>
+                <ul>
+                    <li>진로교육 1년 아카데미 '비전스쿨' 개설</li>
+                    <li className="clear-before">(~2019년 12월 현재 437명 훈련 중)</li>
+                </ul>
+                </div>
             </div>
-            <div className="yearBox left">
-            <h2>2013</h2>
-            <ul>
-                <li>진로교육 1년 아카데미 '비전스쿨' 개설</li>
-                <li>(~2019년 12월 현재 437명 훈련 중)</li>
-            </ul>
+            <div className="timeline-container right">
+                <div className="timeline-content">
+                <h2>2014</h2>
+                <ul>
+                    <li>경기도 교육청 진로멘토단 선정</li>
+                    <li>삼성 SDI 청소년 직업멘토단 활동</li>
+                    <li>전국 직업인 멘토단 창단</li>
+                    <li>창의력 1년 아카데미 '패러다임스쿨' 개설</li>
+                </ul>
+                </div>
             </div>
-            <div className="yearBox right">
-            <h2>2014</h2>
-            <ul>
-                <li>경기도 교육청 진로멘토단 선정</li>
-                <li>삼성 SDI 청소년 직업멘토단 활동</li>
-                <li>전국 직업인 멘토단 창단</li>
-                <li>창의력 1년 아카데미 '패러다임스쿨' 개설</li>
-            </ul>
+            <div className="timeline-container left">
+                <div className="timeline-content">
+                <h2>2015</h2>
+                <ul>
+                    <li>싱가폴 MICROSOFT 직원 직무교육 운영</li>
+                    <li>굿네이버스 취약계층 진로교육 멘토단 운영</li>
+                    <li>경제교육 1년 아카데미 '경제스쿨' 개설</li>
+                </ul>
+                </div>
             </div>
-            <div className="yearBox left">
-            <h2>2015</h2>
-            <ul>
-                <li>싱가폴 MICROSOFT 직원 직무교육 운영</li>
-                <li>굿네이버스 취약계층 진로교육 멘토단 운영</li>
-                <li>경제교육 1년 아카데미 '경제스쿨' 개설</li>
-            </ul>
+            <div className="timeline-container right">
+                <div className="timeline-content">
+                <h2>2016</h2>
+                <ul>
+                    <li>교육부 진로교육 정보망 상담교사 활동</li>
+                    <li>기업가양성 1년 아카데미 '창업스쿨' 개설</li>
+                </ul>
+                </div>
             </div>
-            <div className="yearBox right">
-            <h2>2016</h2>
-            <ul>
-                <li>교육부 진로교육 정보망 상담교사 활동</li>
-                <li>기업가양성 1년 아카데미 '창업스쿨' 개설</li>
-            </ul>
+            <div className="timeline-container left">
+                <div className="timeline-content">
+                <h2>2017</h2>
+                <ul>
+                    <li>한국 진로직업협회 이사 취임</li>
+                    <li>MENTORING-LAB 서울 본사 개소</li>
+                </ul>
+                </div>
             </div>
-            <div className="yearBox left">
-            <h2>2017</h2>
-            <ul>
-                <li>한국 진로직업협회 이사 취임</li>
-                <li>MENTORING-LAB 서울 본사 개소</li>
-            </ul>
-            </div>
-            <div className="yearBox right">
-            <h2>2018</h2>
-            <ul>
-                <li>성신여자대학교 창업교육 학과 개설</li>
-                <li>초기창업패키지 창업과정 4개 아이템 선정</li>
-            </ul>
+            <div className="timeline-container right">
+                <div className="timeline-content">
+                <h2>2018</h2>
+                <ul>
+                    <li>성신여자대학교 창업교육 학과 개설</li>
+                    <li>초기창업패키지 창업과정 4개 아이템 선정</li>
+                </ul>
+                </div>
             </div>
             <div className="gray">OUR STORY WILL GO ON</div>
+            </div>
         </div>
         </div>
         <SubCircle />
--- src/pages/School.js
+++ src/pages/School.js
@@ -7,7 +7,7 @@ import ParadigmSchool from '../components/ParadigmSchool';
 import EconomicSchool from '../components/EconomicSchool';
 import StartupSchool from '../components/StartupSchool';
 
-function School() {
+function School({match}) {
     const title = "SHCOOL";
     const menu = [
         {text : "비전스쿨", url : "/program/school/vision"},
@@ -15,15 +15,26 @@ function School() {
         {text : "경제스쿨", url : "/program/school/economic"},
         {text : "창업스쿨", url : "/program/school/startup"}
     ];
+    const activePage = match.params.keyword;
+    console.log(activePage);
 
   return (
     <main>
         <SubTitle title={title} />
         <div className="school">
         <div className="contents">
-            School 윗부분 내용
+            <h2>매월 격주마다 2회, 1년간 운영되는 아카데미<br />
+            오프라인 토론과 미션들을 통해 생각하며 배웁니다</h2>
+            <p className="par">한국의 교육은 주로 지식적으로 배우는 학(學) 위주로 배웁니다.<br />
+            하지만 유럽과 미국은 이 학과 습(習)을 병행하며 배웁니다.<br />
+            몸에 익혀지는 시간을 통해 자신의 적성과 강점에 맞는지를<br />
+            검증하는 과정이 이미 교육과정의 기초에 들어가 있는 것이죠.</p>
+            <p className="par">진로, 창의력, 경제, 창업···<br />
+            어느 하나 쉽지 않은 영역이지만 학과 습의 병행과 함께<br />
+            그룹토론 수업으로 밀도있게 공부하면 그 동안 알지 못했던<br />
+            새로운 배움의 장(場)을 경험할 수 있습니다.</p>
 
-            <ul class="schoolMenu">
+            <ul className="school-menu">
             {menu.map((item, index)=>{
                 return <li key={index}><a href={item.url}>{item.text}</a></li>
             })}
