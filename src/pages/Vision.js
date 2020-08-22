94bc5b455afa70c6e810ab28e69029a558279006
JinjuNa <48050357+JinjuNa@users.noreply.github.com>
2020-08-22 21:18:11 +0900

    vision responsive-br

--- src/pages/Vision.js
+++ src/pages/Vision.js
@@ -31,13 +31,13 @@ function Vision() {
                     <br />자립형 인간 양성
                 </div>
             </div>
-            <p className="par">사람들에게 잘 보이기 위한 삶에서 벗어나서<br/>
+            <p className="par">사람들에게 잘 보이기 위한 삶에서 벗어나서 <br className="responsive-br"/>
                 자신만의 삶을 살아가려면 자신만의 기준이 필요합니다.<br/><br/>
-                그래서 Mentoring-Lab에서는 자기경영, 창의력, 경제교육, 창업가육성을<br/>
+                그래서 Mentoring-Lab에서는 자기경영, 창의력, 경제교육, 창업가육성을 <br className="responsive-br"/>
                 통해 자신의 길을 스스로 찾아가는 힘을 훈련하고 있습니다.<br/><br/>
-                Mentoring-Lab은 나아가 유럽, 일본, 프랑스의 좋은 진로교육제도를<br/>
+                Mentoring-Lab은 나아가 유럽, 일본, 프랑스의 좋은 진로교육제도를 <br className="responsive-br"/>
                 도입하여 국내 유일의 '진로교육대학'을 설립하기 위해 힘쓰고 있습니다.<br/><br/>
-                모든 인생이 탁월한 인생으로 살아갈 때 까지 Mentoring-Lab의<br/>
+                모든 인생이 탁월한 인생으로 살아갈 때 까지 Mentoring-Lab의 <br className="responsive-br"/>
                 교육실험은 계속됩니다.
             </p>
             <div className="button"><a href="/">회사소개서 다운로드</a></div>
